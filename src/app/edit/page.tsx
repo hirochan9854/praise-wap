d92f5c0c494e07700679d1cd681c1930aeed7db7
hirochan9854 <ito.hiro9854@gmail.com>
2024-10-25 12:07:57 +0900

    リセット

---
+++ src/app/edit/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="w-full">
+      <div>
+        <h2>セリフを編集</h2>
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,3 @@
-import SentimentAnalyzer from '@/components/SentimentAnalyzer';
-import { Transcription } from '@/components/Transcription';
-
 export default function Home() {
-  return (
-    <div>
-      <Transcription />
-      <SentimentAnalyzer />
-    </div>
-  );
+  return <div></div>;
 }
