388d9180df5b0a12e0ec473cac3a276f2b8522f7
hirochan9854 <ito.hiro9854@gmail.com>
2024-12-20 12:20:42 +0900

    タイトル作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,41 @@
 'use client';
 
-import React, { useState } from 'react';
-
-import SentimentAnalyzer from '@/components/SentimentAnalyzer';
-import { Transcription } from '@/components/Transcription';
+import { useRouter } from 'next/navigation';
+import React, { useEffect } from 'react';
 
 export default function Home() {
-  const [newSentence, setNewSentence] = useState<string>('');
-  const handleSentencesUpdate = (sentences: string[]) => {
-    console.log('sentences:', sentences);
-    if (sentences[sentences.length - 1]) {
-      setNewSentence(sentences[sentences.length - 1] || ''); // 配列の最後の文を取得して保存
-    }
-  };
+  // const [newSentence, setNewSentence] = useState<string>('');
+  // const handleSentencesUpdate = (sentences: string[]) => {
+  //   console.log('sentences:', sentences);
+  //   if (sentences[sentences.length - 1]) {
+  //     setNewSentence(sentences[sentences.length - 1] || ''); // 配列の最後の文を取得して保存
+  //   }
+  // };
 
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === ' ') {
+        router.push('/game');
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
-    <div>
+    <div className="flex h-screen flex-col items-center justify-center">
       {/* Transcriptionコンポーネントに文字列の更新を受け取るコールバックを渡す */}
-      <Transcription onSentencesUpdate={handleSentencesUpdate} />
+      {/* <Transcription onSentencesUpdate={handleSentencesUpdate} /> */}
       {/* SentimentAnalyzerに新しい文を渡して解析 */}
-      <SentimentAnalyzer sentence={newSentence} />
+      {/* <SentimentAnalyzer sentence={newSentence} /> */}
+
+      <h1 className="text-7xl ">Prase Wap</h1>
+      <p className="mt-20">スペースキーを押してスタート</p>
     </div>
   );
 }
